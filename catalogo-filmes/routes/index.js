66763ca7aa7156b1a51f3f52fbc9fa0300d161b4
gcapoia <gcapoia@gmail.com>
2015-08-05 21:42:10 -0300

    Create project

---
+++ catalogo-filmes/routes/index.js
@@ -0,0 +1,20 @@
+exports.index = function(req, res){
+	res.render('index');
+}
+
+exports.lista = function(req, res) {
+	res.json({
+		titulo: 'Gangues de Nova York',
+		diretor: 'Martin Scorsese',
+		ano: 2002
+	});
+};
+
+exports.grava = function(req, res){
+	var filme = req.body;
+
+	console.log(filme);
+
+	res.send('Filme ' + filme.titulo + ' recebido no servidor');
+
+}
